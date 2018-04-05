40810f03ab8858306be0f546993cf95eda88e10b
techiemmk <techiemmk@gmail.com>
2018-04-05 22:05:35 +0530

    Weather App sample
    Initial working version of weather app

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    mode: "development",
+    entry: ['./app/index.js'],
+    output: {
+        path: __dirname+"/build",
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: [
+                    __dirname+"/node_modules"
+                ],
+                loader: "babel-loader",
+                options: {
+                    presets: ["es2015", "react"]
+                },
+            }
+        ],
+    },
+    devServer: {
+        port: 3322,
+        contentBase: __dirname+"/",
+        hot: false
+    }
+}

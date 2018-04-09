ecbcff947596139b5a75e1e6156653ec5dd1fea0
techiemmk <techiemmk@gmail.com>
2018-04-09 13:15:50 +0530

    for fixing the port issue

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,7 @@
 const path = require('path')
 
 module.exports = {
-    mode: 'development',
-    entry: ['./src/index.js'],
+    entry: ['./src/index.js'],    
     output: {
         path: path.join(__dirname, '/build'),
         filename: 'bundle.js'
@@ -20,5 +19,5 @@ module.exports = {
                 },
             }
         ],
-    }
+    },
 }

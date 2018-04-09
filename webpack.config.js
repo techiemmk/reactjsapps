a30ba13031c9a87b97151e308553dc5ee99fd878
techiemmk <techiemmk@gmail.com>
2018-04-09 15:09:56 +0530

    for fixing the port issue

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,20 @@
 const path = require('path');
+const webpack = require('webpack');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 const PORT = process.env.PORT || 1122;
 const HOST = process.env.HOST || "127.0.0.1";
 module.exports = {
-    entry: ['./src/index.js'],    
+    devtool: 'cheap-module-source-map',
+    entry: ['./src/index.js'],
+    plugins: [
+        new UglifyJSPlugin({sourceMap: true}),
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('production')
+            }
+        })
+    ],
     output: {
         path: path.join(__dirname, '/build'),
         filename: 'bundle.js'
@@ -23,7 +34,6 @@ module.exports = {
         ],
     },
     devServer: {
-        host: HOST,
         port: PORT
     }
 }
